11839638e433a04655bfa3d05a4f302a18e0c8f4
SpatLyu <lyu.geosocial@gmail.com>
2024-08-30 14:29:34 +0800

    update

--- DOI_2_R_PACKAGE_CITATION.R
+++ DOI_2_R_PACKAGE_CITATION.R
@@ -2,4 +2,5 @@ doi2rpkgcita = \(doi){
   return(rcrossref::cr_cn(dois = doi, format = "bibentry"))
 }
 
-doi2rpkgcita("10.1007/s11004-022-10036-8")
+doi2rpkgcita("10.1007/s11004-022-10036-8") # geosimilarity
+doi2rpkgcita("10.1016/j.jag.2022.102834") # SecDim

924d16e63df2471fe37f0365a0e1f499499ac5ca
SpatLyu <lyu.geosocial@gmail.com>
2024-10-27 23:18:20 +0800

    add script

---
+++ Add_Favicons.R
@@ -0,0 +1,26 @@
+fs::file_copy("./pkgdown/favicon/apple-touch-icon.png",
+              "./pkgdown/favicon/apple-touch-icon-180x180.png", overwrite = TRUE)
+
+magick::image_read("./pkgdown/favicon/apple-touch-icon-180x180.png") |>
+  magick::image_resize("152x152")|>
+  magick::image_write("./pkgdown/favicon/apple-touch-icon-152x152.png")
+
+magick::image_read("./pkgdown/favicon/apple-touch-icon-180x180.png") |>
+  magick::image_resize("120x120")|>
+  magick::image_write("./pkgdown/favicon/apple-touch-icon-120x120.png")
+
+magick::image_read("./pkgdown/favicon/apple-touch-icon-180x180.png") |>
+  magick::image_resize("76x76")|>
+  magick::image_write("./pkgdown/favicon/apple-touch-icon-76x76.png")
+
+magick::image_read("./pkgdown/favicon/apple-touch-icon-180x180.png") |>
+  magick::image_resize("60x60")|>
+  magick::image_write("./pkgdown/favicon/apple-touch-icon-60x60.png")
+
+magick::image_read("./pkgdown/favicon/favicon-48x48.png") |>
+  magick::image_resize("32x32")|>
+  magick::image_write("./pkgdown/favicon/favicon-32x32.png")
+
+magick::image_read("./pkgdown/favicon/favicon-48x48.png") |>
+  magick::image_resize("16x16")|>
+  magick::image_write("./pkgdown/favicon/favicon-16x16.png")

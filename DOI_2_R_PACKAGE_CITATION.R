30cc956ab0afbae80216b8118a9cacf540a35639
SpatLyu <lyu.geosocial@gmail.com>
2025-01-19 23:36:29 +0800

    add `localsp` pkg

--- DOI_2_R_PACKAGE_CITATION.R
+++ DOI_2_R_PACKAGE_CITATION.R
@@ -4,3 +4,4 @@ doi2rpkgcita = \(doi){
 
 doi2rpkgcita("10.1007/s11004-022-10036-8") # geosimilarity
 doi2rpkgcita("10.1016/j.jag.2022.102834") # SecDim
+doi2rpkgcita("10.1080/13658816.2024.2437811") # localsp
